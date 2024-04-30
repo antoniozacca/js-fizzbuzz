7f319929e5623919ac0f0368d0aefa7c906280b9
antoniozacca <antoniozaccaria057@gmail.com>
2024-04-30 17:25:42 +0200

    finish exercise

--- main.js
+++ main.js
@@ -19,18 +19,18 @@ const ul = document.querySelector("ul.list");
 for (let i = 1; i<=100; i++) {
     //stampa in html FizzBuzz al posto di tutti i multipli di 3 e di 5
     if(i % 3 === 0 && i % 5 === 0){
-        const element = `<li class="box col-md-3 col-sm-6 col-xs-6 p-3 text-center bg-bright-pink-crayola box--${i}">FizzBuzz</li>`
+        const element = `<li class="box p-3 text-center bg-bright-pink-crayola box--${i}">FizzBuzz</li>`
     ul.innerHTML += element;
         //stampa in html Fizz al posto di tutti i multipli di 3
     } else if (i % 3 === 0) {
-        const element = `<li class="box col-md-3 col-sm-6 col-xs-6 p-3 text-center bg-emerald box--${i}">Fizz</li>`
+        const element = `<li class="box p-3 text-center bg-emerald box--${i}">Fizz</li>`
     ul.innerHTML += element;
         //stampa in html Buzz al posto di tutti i multipli di 5
     } else if (i % 5 === 0) {
-        const element = `<li class="box col-md-3 col-sm-6 col-xs-6 p-3 text-center bg-sunglow box--${i}">Buzz</li>`
+        const element = `<li class="box p-3 text-center bg-sunglow box--${i}">Buzz</li>`
     ul.innerHTML += element;
     } else{
-        const element = `<li class="box col-md-3 col-sm-6 col-xs-6 p-3 text-center bg-blue-ncs box--${i}">${i}</li>`
+        const element = `<li class="box p-3 text-center bg-blue-ncs box--${i}">${i}</li>`
         ul.innerHTML += element;
     }
     
