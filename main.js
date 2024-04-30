ee3c76d36d509cf5596fdd2e4d1f11ca27ad8002
antoniozacca <antoniozaccaria057@gmail.com>
2024-04-30 16:05:53 +0200

    start index.html and js for bonus 1

--- main.js
+++ main.js
@@ -13,8 +13,11 @@ for (let i = 1; i <=100; i++) {
     } else{
         console.log(i);
     }
-
-    
-
-    
 }
+
+// const ul = document.querySelector("ul.list");
+// for (let i = 1; i<=100; i++) {
+//     const element = `<li class="box box--${i}">${i}</li>`
+//     ul.innerHTML += element;
+    
+// }
