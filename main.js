a728bf777dca18bcd8701d22446462b7e595dfc9
antoniozacca <antoniozaccaria057@gmail.com>
2024-04-30 15:26:49 +0200

    finish exercise and start bonus 1

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 'use strict'
+
+for (let i = 1; i <=100; i++) {
+    //stampa in console FizzBuzz al posto di tutti i multipli di 3 e di 5
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+        //stampa in console Fizz al posto di tutti i multipli di 3
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+        //stampa in console Buzz al posto di tutti i multipli di 5
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else{
+        console.log(i);
+    }
+
+    
+
+    
+}
