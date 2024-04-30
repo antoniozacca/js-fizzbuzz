f36f92a764ffd74d437823b8edde7ac1aaf93859
antoniozacca <antoniozaccaria057@gmail.com>
2024-04-30 16:21:41 +0200

    finish bonus 1

--- main.js
+++ main.js
@@ -15,9 +15,24 @@ for (let i = 1; i <=100; i++) {
     }
 }
 
-// const ul = document.querySelector("ul.list");
-// for (let i = 1; i<=100; i++) {
-//     const element = `<li class="box box--${i}">${i}</li>`
-//     ul.innerHTML += element;
+const ul = document.querySelector("ul.list");
+for (let i = 1; i<=100; i++) {
+    //stampa in html FizzBuzz al posto di tutti i multipli di 3 e di 5
+    if(i % 3 === 0 && i % 5 === 0){
+        const element = `<li class="box box--${i}">FizzBuzz</li>`
+    ul.innerHTML += element;
+        //stampa in html Fizz al posto di tutti i multipli di 3
+    } else if (i % 3 === 0) {
+        const element = `<li class="box box--${i}">Fizz</li>`
+    ul.innerHTML += element;
+        //stampa in html Buzz al posto di tutti i multipli di 5
+    } else if (i % 5 === 0) {
+        const element = `<li class="box box--${i}">Buzz</li>`
+    ul.innerHTML += element;
+    } else{
+        const element = `<li class="box box--${i}">${i}</li>`
+        ul.innerHTML += element;
+    }
     
-// }
+    
+}
